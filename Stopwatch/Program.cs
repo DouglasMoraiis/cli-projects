45e4d9ef35b8b6553acdce2c29ec704831b27027
Jander Morais <jander.morais@fitbank.com.br>
2022-02-25 15:22:13 -0300

    feat: Novos projetos adicionados

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo");
+            Console.WriteLine("M = Minuto");
+            Console.WriteLine("0 = Sair");
+            Console.Write("Quanto tempo deseja contar? ");
+            string data = Console.ReadLine().ToLower();
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+
+            if (type == '0')
+                Environment.Exit(0);
+
+            int multiplier = type == 'm' ? 60 : 1;
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go!");
+            Thread.Sleep(1000);
+            Start(time);
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Stopwatch finalizado!");
+            Thread.Sleep(2500);
+        }
+    }
+}
