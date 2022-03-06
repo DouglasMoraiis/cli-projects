ddaec5a69f3547cf4224dde9147d076449cf22ba
Douglas Morais <douglasmoraisdev@gmail.com>
2022-03-05 23:27:33 -0300

    chore: Instalação e uso do Dapper

--- Balta/BaltaDataAccess/Program.cs
+++ Balta/BaltaDataAccess/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using BaltaDataAcces.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
 namespace BaltaDataAccess
 {
     class Program
@@ -7,20 +9,46 @@ namespace BaltaDataAccess
         {
             const string connectionString = "Server=localhost, 1433;Database=balta;User ID=sa; Password=S=jdm123mdj@";
 
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon.com";
+            category.Summary = "AWS Cloud";
+            category.Order = 8;
+            category.Description = "Categoria destinada a serviços AWS";
+            category.Featured = false;
+
+            //Warning !!! - SQL Injection
+            var insertSql = @"
+                INSERT INTO 
+                    [Category] 
+                VALUES (
+                    @Id, 
+                    @Title, 
+                    @Url, 
+                    @Summary, 
+                    @Order, 
+                    @Description, 
+                    @Featured
+            )";
+            
             using (var connection = new SqlConnection(connectionString))
             {
-                Console.WriteLine("Conectado!");
-                connection.Open();
-                using (var command = new SqlCommand())
+                var rows = connection.Execute(insertSql, new {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured,
+                });
+                Console.WriteLine($"{rows} linhas inseridas.");
+
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var item in categories)
                 {
-                    command.Connection = connection;
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                    }
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
         }
