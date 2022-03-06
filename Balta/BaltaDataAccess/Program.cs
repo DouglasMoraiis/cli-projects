4f0b893a1d6f6105323270216aa9915da570adaa
Douglas Morais <douglasmoraisdev@gmail.com>
2022-03-06 00:06:29 -0300

    chore: Método UpdateCategory foi implementado

--- Balta/BaltaDataAccess/Program.cs
+++ Balta/BaltaDataAccess/Program.cs
@@ -5,10 +5,30 @@ namespace BaltaDataAccess
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             const string connectionString = "Server=localhost, 1433;Database=balta;User ID=sa; Password=S=jdm123mdj@";
 
+            using (var connection = new SqlConnection(connectionString))
+            {
+                ListCategories(connection);
+                UpdateCategory(connection);
+                ListCategories(connection);
+                //CreateCategory(connection);
+            }
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        static void CreateCategory(SqlConnection connection)
+        {
             var category = new Category();
             category.Id = Guid.NewGuid();
             category.Title = "Amazon AWS";
@@ -31,26 +51,38 @@ namespace BaltaDataAccess
                     @Description, 
                     @Featured
             )";
-            
-            using (var connection = new SqlConnection(connectionString))
-            {
-                var rows = connection.Execute(insertSql, new {
-                    category.Id,
-                    category.Title,
-                    category.Url,
-                    category.Summary,
-                    category.Order,
-                    category.Description,
-                    category.Featured,
-                });
-                Console.WriteLine($"{rows} linhas inseridas.");
 
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var item in categories)
-                {
-                    Console.WriteLine($"{item.Id} - {item.Title}");
-                }
-            }
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured,
+            });
+            Console.WriteLine($"{rows} linhas inseridas.");
+        }
+
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = @"
+                UPDATE 
+                    [Category] 
+                SET 
+                    [Title] = @Title 
+                WHERE 
+                    [Id] = @Id
+            ";
+
+            var rows = connection.Execute(updateQuery, new
+            {
+                Id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                Title = "Frontend 2021",
+            });
+
+            Console.WriteLine($"{rows} registros atualizados");
         }
     }
 }
